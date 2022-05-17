1525816e06697c497fb73b1592f62e1a5a2fa19f
Gregor Dirks <gregor_renke_adrian@mailbox.tu-dresden.de>
2022-05-17 13:24:51 +0200

    ersetzt durch java interface test

---
+++ Zahl.java
@@ -0,0 +1,10 @@
+public class Zahl implements Main.X{
+
+        private int a;
+
+        public void addition(){
+                System.out.println(
+                        "Du führst eine Addition aus."
+                );
+        }
+}
